205fd1dc07f8b5d8dca8d47ea2afe89151bdcbb3
Tomoki Shimada <tomoki.shimada0927@gmail.com>
2017-03-18 18:07:13 +0900

    Comment View created

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,36 @@
 class CommentsController < ApplicationController
+
+  before_action :set_comment, only: [:show, :edit, :update, :destroy]
+
+  def create
+    @my_thread = MyThread.find(params[:my_thread_id])
+    @comment = @my_thread.comments.create(comment_params)
+    redirect_to my_thread_path(params[:my_thread_id])
+  end
+
+  def edit
+  end
+
+  def destroy
+    @comment.destroy
+    redirect_to my_thread_path(@comment.my_thread_id)
+  end
+
+  def update
+    if @comment.update(comment_params)
+      redirect_to my_thread_path(@comment.my_thread_id)
+    else
+      render 'show'
+    end
+  end
+
+  private
+    def comment_params
+      params[:comment].permit(:user_id, :body)
+    end
+
+    def set_comment
+      @comment = Comment.find(params[:id])
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :my_threads, except: [:new] do
-    resources :comments, shallow: true, only: [:create, :edit, :update, :delete]
+    resources :comments, shallow: true, only: [:create, :edit, :update, :destroy]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
