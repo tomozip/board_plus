8b68b14989d42e4b9076e1245c7f8cc304c21e8b
Tomoki Shimada <tomoki.shimada0927@gmail.com>
2017-03-18 18:21:53 +0900

    devise setting done

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   resources :my_threads, except: [:new] do
     resources :comments, shallow: true, only: [:create, :edit, :update, :destroy]
   end
+
+  root 'my_threads#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
